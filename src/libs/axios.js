c28cfa94a76ae219f679949f08c41fa0ec4a91a5
mkcui <cuitrek@163.com>
2018-10-29 10:31:14 +0800

    添加注释

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios'
 import store from '@/store'
 // import { Spin } from 'iview'
 const addErrorLog = errorInfo => {
+  // 需要注意的是 此处有一个从属性值中结构的语法
   const { statusText, status, request: { responseURL } } = errorInfo
   let info = {
     type: 'ajax',
@@ -9,9 +10,11 @@ const addErrorLog = errorInfo => {
     mes: statusText,
     url: responseURL
   }
+  // 需要注意的是此处 有一个 类似 indexOf 的方法 查看数组中是否包含某项
   if (!responseURL.includes('save_error_logger')) store.dispatch('addErrorLog', info)
 }
 
+// 此处是构造一个请求类 在类实例化的时候 需要两个参数 一个是请求的基础地址 必填项 一个是请求的参数 非必填项
 class HttpRequest {
   constructor (baseUrl = baseURL) {
     this.baseUrl = baseUrl
@@ -57,6 +60,7 @@ class HttpRequest {
   }
   request (options) {
     const instance = axios.create()
+    // 需要注意的是 此处有一个对象合并的方法 将后面的对象的所有属性合并到第一个对象上面 
     options = Object.assign(this.getInsideConfig(), options)
     this.interceptors(instance, options.url)
     return instance(options)
