2e36a19ae1245a97dd7aa24a86d174212ca71ab9
mkcui <cuitrek@163.com>
2018-10-30 19:59:59 +0800

    格式化当前文件

--- src/view/tools-methods/tools-methods.vue
+++ src/view/tools-methods/tools-methods.vue
@@ -34,49 +34,51 @@
 </template>
 
 <script>
-import { mapMutations } from 'vuex'
-export default {
-  name: 'tools_methods_page',
-  methods: {
-    ...mapMutations([
-      'addTag',
-      'closeTag'
-    ]),
-    createTagParams () {
-      const id = parseInt(Math.random() * 100000)
-      const route = {
-        name: 'params',
-        params: {
-          id
-        },
-        meta: {
-          title: `动态路由-${id}`
+  import {
+    mapMutations
+  } from 'vuex'
+  export default {
+    name: 'tools_methods_page',
+    methods: {
+      ...mapMutations([
+        'addTag',
+        'closeTag'
+      ]),
+      createTagParams() {
+        const id = parseInt(Math.random() * 100000)
+        const route = {
+          name: 'params',
+          params: {
+            id
+          },
+          meta: {
+            title: `动态路由-${id}`
+          }
         }
-      }
-      this.$router.push(route)
-    },
-    createTagQuery () {
-      const id = parseInt(Math.random() * 100000)
-      const route = {
-        name: 'query',
-        query: {
-          id
-        },
-        meta: {
-          title: `参数-${id}`
+        this.$router.push(route)
+      },
+      createTagQuery() {
+        const id = parseInt(Math.random() * 100000)
+        const route = {
+          name: 'query',
+          query: {
+            id
+          },
+          meta: {
+            title: `参数-${id}`
+          }
         }
+        this.$router.push(route)
+      },
+      handleCloseTag() {
+        this.closeTag({
+          name: 'tools_methods_page'
+        })
       }
-      this.$router.push(route)
-    },
-    handleCloseTag () {
-      this.closeTag({
-        name: 'tools_methods_page'
-      })
     }
   }
-}
 </script>
 
 <style>
-
+  
 </style>
